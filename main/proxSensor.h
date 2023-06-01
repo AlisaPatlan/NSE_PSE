418133a091d7d7d8a1827bc0d4fe025044a30ece
Alisa <57398479+AlisaPatlan@users.noreply.github.com>
2023-06-01 13:39:42 +0200

    Update proxSensor.h
    Ik heb wat aan header-file van de prox sensors aan de repo toegevoegd 
    -Alisa

--- main/proxSensor.h
+++ main/proxSensor.h
@@ -1,8 +1,19 @@
-class proxSensor{
 
+#ifndef PROXSENSOR_H
+#define PROXSENSOR_H
+#include <Wire.h>
+#include <Zumo32U4.h>
+#include <Zumo32U4ProximitySensors.h>
+class proxSensor:public Sensors
+{
   public:
-
+  proxSensors();
+  ~proxSensors();
+  int read();
+  int SensorStatus();
+  
   private:
-
+ bool detectedObstacle;
+  int statusProxSensor;
 
 };
