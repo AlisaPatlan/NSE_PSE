12dcdb50e06a745d13e4b69dfc6f5f71835299d9
arjan <arjankuiper98@gmail.com>
2023-06-09 10:18:38 +0100

    Main switch tussen zelf rijden en sensor lezen

--- main/main.ino
+++ main/main.ino
@@ -3,42 +3,79 @@
 #include "xBee.h"
 #include "proxSensor.h"
 #include "Accelerometer.h"
-
+#include <Wire.h>
 using namespace std;
 MotorsControler ZumoMotor;
 LineSensor LineSensors;
 Accelerometer accMeter;
 xBee xBeeConnectie;
 proxSensor proxSensor;
+Zumo32U4ButtonA buttonA;
+Zumo32U4ButtonB buttonB;
+
+String gedrukteKnop = "";
+bool knopGedrukt = false;
 
 
 void setup()
 {
   Serial.begin(9000);
+  do{
+  if(buttonA.isPressed()){
+    gedrukteKnop = "a";
+    knopGedrukt = true;
+  }
+  if(buttonB.isPressed()){
+    gedrukteKnop = "b";
+    knopGedrukt = true;
+  }
+  } while (!knopGedrukt);
+
 }
 
 void loop()
 {
-  LineSensors.leesWaarde();
-  /* als de sensor bruine dwarsliggende lijn detecteert rijdt hij 20 cm naar voren
-      en doet 3 pogingen om het blokje te detecteren.
-      ZUMO draait om zijn as en gebruikt proximity sensor.
-      Als hij het blokje detecteert, rijdt hij naar voren op hoge snelheid.
-      */
-    bool bruinGezien = LineSensors.bruinBeideGezien();
-    if (bruinGezien){
-      ZumoMotor.startRijdenCirkel(200,200);
+  if(gedrukteKnop == "a"){
+      LineSensors.leesWaarde();
+      bool bruinGezien = LineSensors.bruinBeideGezien();
+      if (bruinGezien){
+        ZumoMotor.startRijdenCirkel(200,200);
+      }
+      ZumoMotor.startRijden(200,0);  
+      bool obstakel = proxSensor.Obstakel();
+      if (obstakel == true) {
+        ZumoMotor.stopRijden();
+        delay(100);
+        ZumoMotor.startRijden(400,400);
+      }
+      else{
+        ZumoMotor.startRijden(200,0);
+      }
+  }
+  if(gedrukteKnop == "b"){
+    if (Serial1.available()){
+    char command = Serial1.read();
+    Serial1.write(command);
+     switch (command) {
+      case 'W': // Forward
+        ZumoMotor.startRijden(200, 200); // Adjust the motor speeds as needed
+        break;
+      
+      case 'S': // Backward
+        ZumoMotor.startRijden(-200, -200); // Adjust the motor speeds as needed
+        break;
+      
+      case 'A': // Turn left
+        ZumoMotor.startRijden(-200, 200); // Adjust the motor speeds as needed
+        break;
+      
+      case 'D': // Turn right
+        ZumoMotor.startRijden(200, -200); // Adjust the motor speeds as needed
+        break;
+      case 'X': // Stop
+        ZumoMotor.stopRijden();
+        break;
     }
-  ZumoMotor.startRijden(200,0);  
-  bool obstakel = proxSensor.Obstakel();
-  if (obstakel == true) {
-    ZumoMotor.stopRijden();
-    delay(100);
-    ZumoMotor.startRijden(400,400);
   }
-  else{
-    ZumoMotor.startRijden(200,0);
   }
-
-  
 }
