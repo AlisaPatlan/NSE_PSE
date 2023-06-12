d6a149ae9a077792f7a3dcd3d71673693971fb16
israalol <22105190@student.hhs.nl>
2023-06-12 17:38:24 +0200

    autonoom v2

---
+++ main/autonoom.h
@@ -0,0 +1,37 @@
+#ifndef AUTONOOM_H
+#define AUTONOOM_H
+
+#include "LineSensor.h"
+#include "proxSensor.h"
+#include "MotorsControler.h"
+#include "Accelerometer.h"
+#include "xBee.h"
+
+class autonoom{
+  public:
+  autonoom();
+  void initSensor();
+  int leesSensor();
+  void startRijden(int, int);
+  void stopRijden();
+  void startRijdenCirkel(int, int);
+  void InitSensors();
+  bool Obstakel();
+  void volgLijnp();
+  void calibrateSensors();
+  void leesWaarde();
+  void leesKleurWaarde();
+  bool bruinBeideGezien();
+
+
+
+  private:
+  MotorsControler ZumoMotor;
+  LineSensor LineSensors;
+  Accelerometer accMeter;
+  ZumoRobot zumo;
+  proxSensor proxSensor;
+  Zumo32U4ButtonA buttonA;
+  Zumo32U4ButtonB buttonB;
+
+}
